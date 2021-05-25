267120adf098adbb7c9c80d52e811d80ce374cfb
Eduardo Torres <eduk_1012@hotmail.com>
2021-05-25 01:24:52 -0500

    Headless wordpress project

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <ul>
+        <li v-for="blog in blogs" :key="blog.id">
+          {{blog.title.rendered}}
+          <NuxtLink  :to="`posts/${blog.id}`">
+            Ver más
+          </NuxtLink>
+
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+async asyncData({ $axios }) {
+  const blogs = await $axios.$get('http://localhost/TemplateEnBlancoHeadless/wp-json/wp/v2/posts')
+  return { blogs }
+}
+
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>{{post.title.rendered}}</h2>
+            </div>
+            <div class="col-lg-12">
+                <div class="content" v-html="post.content.rendered"></div>
+            </div>
+        </div>
+        
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        async asyncData({params, $axios}){
+            
+            const post = (await $axios.get(`http://localhost/TemplateEnBlancoHeadless/wp-json/wp/v2/posts/${params.id}`)).data
+            
+            return { 
+                post
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.content >>> img{
+        width: 100%;
+    }
+</style>
